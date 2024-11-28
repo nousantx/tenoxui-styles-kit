6179a13af0d6392681f803cb1ea46a3c0a8caef2
nousantx <nousantx@gmail.com>
2024-11-28 20:43:33 +0700

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-// index.ts
-import { merge, transformClasses } from '@nousantx/someutils'
-import type { Classes } from '@tenoxui/core'
-import tx from './utils/assigner'
+import { toChildString } from '@nousantx/someutils'
+import { createComponents } from './utils/create-components'
+import styles from './styles'
 
-const button = tx.button()
+const components = createComponents(styles)
 
-export const stringTemplate: string = `
-  ${button.template}
-  (.badge): bg-red-400;
-`
-
-export const styles: Classes = merge(button.utility, transformClasses(button.classes))
-
-export default {
-  stringTemplate,
-  styles
+export const templates = {
+  // all combined strings
+  all: toChildString(
+    Object.values(components).reduce<Record<string, string>>((acc, curr) => {
+      return { ...acc, ...curr }
+    }, {})
+  ),
+  // each strings generated
+  ...Object.entries(components).reduce(
+    (acc, [key, value]) => {
+      acc[key as keyof typeof styles] = toChildString(value)
+      return acc
+    },
+    {} as Record<keyof typeof styles, string>
+  )
 }
+
+export default templates
