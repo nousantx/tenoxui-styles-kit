055efdf1567af0838ccf86f642bf048da9b574ee
nousantx <nousantx@gmail.com>
2024-11-30 09:28:14 +0700

    Use tenoxui aliases instead of class selector

--- src/styles/badge.ts
+++ src/styles/badge.ts
@@ -1,12 +1,14 @@
 export const badge = (className: string) => ({
-  // baee
-  [`.${className}`]:
-    'center h-28px fs-1rem px-10px br-999rem text-neutral-950 bg-neutral-100 ls--0.015em lh-1 fw-500',
+  // base
+  [`${className}`]:
+    'd-flex flex-parent-center h-26px px-8px fs-0.875rem br-4px text-neutral-950 bg-neutral-100 ls--0.015em lh-1 fw-500 bg-opacity-0.3',
   // sizes
-  [`.${className}.${className}-xs`]: 'h-22px px-6px fs-0.75rem',
-  [`.${className}.${className}-sm`]: 'h-25px px-8px fs-0.875rem',
+  [`${className}-sm`]: 'h-22px px-6px fs-0.75rem',
+  [`${className}-lg`]: 'h-28px px-10px fs-1rem',
+  [`${className}-round`]: 'br-999rem px-12px',
   // variant example
-  [`.${className}.${className}-danger`]: 'text-red-700 bg-red-100',
-  [`.${className}.${className}-approve`]: 'text-blue-800 bg-blue-200',
-  [`.${className}.${className}-success`]: 'text-green-700 bg-green-100'
+  [`${className}-danger`]: '[text,bdr-c]-red-600 bg-red-400',
+  [`${className}-approve`]: '[text,bdr-c]-blue-800 bg-blue-200',
+  [`${className}-success`]: '[text,bdr-c]-green-500 bg-green-300',
+  [`${className}-border`]: 'bw-1px bs-solid [--bdr-c-opacity]-0.3'
 })
