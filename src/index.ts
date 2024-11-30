9acbeb1178aeab0319a62ffea77cd6bcded33e47
nousantx <nousantx@gmail.com>
2024-11-30 09:28:34 +0700

    Pass build

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,19 @@
-import { toChildString } from '@nousantx/someutils'
 import { createComponents } from './utils/create-components'
 import styles from './styles'
 
-const components = createComponents(styles)
+export const components = createComponents(styles)
 
 export const templates = {
-  // all combined strings
-  all: toChildString(
-    Object.values(components).reduce<Record<string, string>>((acc, curr) => {
-      return { ...acc, ...curr }
-    }, {})
-  ),
-  // each strings generated
+  all: Object.values(components).reduce<Record<string, string>>((acc, curr) => {
+    return { ...acc, ...curr }
+  }, {}),
   ...Object.entries(components).reduce(
     (acc, [key, value]) => {
-      acc[key as keyof typeof styles] = toChildString(value)
+      acc[key as keyof typeof styles] = Object.values(value).join(' ')
       return acc
     },
     {} as Record<keyof typeof styles, string>
   )
 }
 
-export default templates
+export default { templates, components }
