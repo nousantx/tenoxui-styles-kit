daddf90a292995752ba9cf236e481a1be48c4c59
nousantx <nousantx@gmail.com>
2024-11-30 09:29:18 +0700

    Add more components

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import { templates } from './dist/index.esm.js'
+import { toChildString, fromChildString } from '@nousantx/someutils'
+import { templates, components } from './dist/index.esm.js'
 
 console.log(templates.all)
+console.log(templates.button)
