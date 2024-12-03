34f84b5ef6dc1530685bf8da370e5fc10cb1f34c
nousantx <nousantx@gmail.com>
2024-12-03 19:37:56 +0700

    Run prettier

--- src/styles/display.ts
+++ src/styles/display.ts
@@ -1,9 +1,5 @@
-
-
-
-
-export const display = (className: string) =>({
-  flex:"d-flex",
-  "flex-center":"d-flex [ai-jc]-center",
-  "grid":"d-grid",
+export const display = (className: string) => ({
+  flex: 'd-flex',
+  'flex-center': 'd-flex [ai-jc]-center',
+  grid: 'd-grid'
 })
