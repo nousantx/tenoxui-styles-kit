8c83d1dfc8aa4480b8bc7a5b37b72e35f3ab94f1
nousantx <nousantx@gmail.com>
2024-12-14 16:57:46 +0700

    Update index file

--- packages/tenoxui-plugin/src/index.ts
+++ packages/tenoxui-plugin/src/index.ts
@@ -1,10 +1,18 @@
 import type { CoreConfig } from '@tenoxui/core/full'
 import { border } from './border'
 import { boxShadow } from './boxShadow'
+import { ring } from './ring'
+import { gridSystem } from './grid'
+import { transform } from './transform'
+import { filter } from './filter'
 
 export const plugins: Record<string, CoreConfig> = {
   border,
-  boxShadow
+  boxShadow,
+  ring,
+  gridSystem,
+  transform,
+  filter
 }
 
-export default plugins
+export default { border, boxShadow, ring, gridSystem, transform, filter }
