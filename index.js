174fb5904bd7952b62f51169b08e37e4258d46af
nousantx <nousantx@gmail.com>
2024-12-14 16:03:28 +0700

    Remove old files

--- index.js
+++
@@ -1,5 +0,0 @@
-import { toChildString, fromChildString } from '@nousantx/someutils'
-import { templates, components } from './dist/index.esm.js'
-
-console.log(templates.all)
-console.log(templates.button)
